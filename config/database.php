d6d88872a03b18fcf256196bc261d599698c3799
LucaseNogueira <31225045+LucaseNogueira@users.noreply.github.com>
2024-07-31 03:18:15 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL', ''),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -76,7 +76,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
-            'options' => env('DB_OPTIONS', '')
         ],
 
         'sqlsrv' => [
